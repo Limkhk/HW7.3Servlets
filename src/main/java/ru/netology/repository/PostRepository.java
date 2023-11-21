3e18d556341be446daea984c4346f2b183c1be7c
Averkin S.S <ssaverkin@ya.ru>
2023-11-21 20:39:27 +0300

    add feature repo + controller

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,41 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong idCounter = new AtomicLong(0);
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
 
-  public void removeById(long id) {
-  }
+    public Post save(Post post) throws NotFoundException {
+        if (post.getId() == 0) {
+            post.setId(idCounter.incrementAndGet());
+            posts.put(post.getId(), post);
+        } else {
+            if (posts.containsKey(post.getId())) {
+                posts.replace(post.getId(), post);
+            } else {
+                throw new NotFoundException("not found");
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
